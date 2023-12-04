78398a9b2ae4268cce5e0e322a930610bf7942b3
AidanGrimmettSwin <141190734+AidanGrimmettSwin@users.noreply.github.com>
2023-12-04 20:16:13 +1100

    added final funky donut from mathematical surfaces

--- Basics/MathematicalSurfaces/MathematicalSurfaces/Assets/Scripts/FunctionLibrary.cs
+++ Basics/MathematicalSurfaces/MathematicalSurfaces/Assets/Scripts/FunctionLibrary.cs
@@ -6,9 +6,9 @@ public static class FunctionLibrary
     //Delegates are references to methods. 
     public delegate Vector3 Function(float u, float v, float t);
     //we can make an array of delegates, so that different methods can easily be invoked using the array (
-    static Function[] functions = { Wave, MultiWave, Ripple, Sphere, FunkySphere};
+    static Function[] functions = { Wave, MultiWave, Ripple, Sphere, FunkySphere, Donut, FunkyDonut};
 
-    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, FunkySphere }
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, FunkySphere, Donut, FunkyDonut }
 
     public static Function GetFunction(FunctionName name)
     {
@@ -60,11 +60,37 @@ public static class FunctionLibrary
 
     public static Vector3 FunkySphere(float u, float v, float t)
     {
-        float r = 0.9f + 0.1f * Sin(PI * (4f * u + 8f * v + t));
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
         float s = r * Cos(PI * 0.5f * v);
         Vector3 p;
         p.x = s * Sin(PI * u);
         p.y = r * Sin((PI * 0.5f) * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }   
+    
+    public static Vector3 Donut(float u, float v, float t)
+    {
+        float r1 = 0.75f + Sin(PI * t);
+        float r2 = 0.25f + Sin(PI * t);
+        float s = r1 + r2 * Cos(PI * v);
+
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 FunkyDonut(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6 * u + (0.5f * t)));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8 * u + 4 * v + 2 * t));
+        float s = r1 + r2 * Cos(PI * v);
+
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
     }
