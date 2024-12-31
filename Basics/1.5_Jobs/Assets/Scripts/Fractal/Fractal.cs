31c400e44aae422118bff8ba23e659115136e1cf
AidanGrimmettSwin <141190734+AidanGrimmettSwin@users.noreply.github.com>
2024-12-31 11:48:10 +1100

    completed next stage, creating objects and manually controlling the position and rotations with flat hierarchy
    continue from section 3 Procedural drawing

--- Basics/1.5_Jobs/Assets/Scripts/Fractal/Fractal.cs
+++ Basics/1.5_Jobs/Assets/Scripts/Fractal/Fractal.cs
@@ -6,7 +6,7 @@ public class Fractal : MonoBehaviour
     struct FractalPart
     {
         public Vector3 direction;
-        public Vector3 rotation;
+        public Quaternion rotation;
         public Transform transform;
     }
 
@@ -30,12 +30,20 @@ public class Fractal : MonoBehaviour
         Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f) 
     };
 
-    void CreatePart(int levelIndex, int childIndex, float scale)
+    FractalPart CreatePart(int levelIndex, int childIndex, float scale)
     {
         GameObject part = new GameObject("Fractal Part L" + levelIndex + " C" + childIndex);
+        part.transform.localScale = scale * Vector3.one;
         part.transform.SetParent(transform);
         part.AddComponent<MeshFilter>().mesh = mesh;
         part.AddComponent<MeshRenderer>().material = material;
+
+        return new FractalPart
+        {
+            direction = directions[childIndex],
+            rotation = rotations[childIndex],
+            transform = part.transform
+        };
     }
 
     private void Awake()
@@ -50,7 +58,7 @@ public class Fractal : MonoBehaviour
         }//each level of the parts array has a FractalPart[] which has 5 times the points as the last level
 
         float scale = 1f;
-        CreatePart(0, 0, scale);
+        parts[0][0] = CreatePart(0, 0, scale);
         //li = level index
         //fpi = fractal part index
         //ci = child index
@@ -62,8 +70,36 @@ public class Fractal : MonoBehaviour
             {
                 for (int ci = 0; ci < 5; ci++) //create 5 parts, child index will help distinguish between directions
                 {
-                    CreatePart(li, ci, scale);
+                    levelParts[fpi + ci] = CreatePart(li, ci, scale);
                 }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+
+        FractalPart rootPart = parts[0][0];
+        rootPart.rotation *= deltaRotation; //changing local struct variable value will not change the array element
+        rootPart.transform.localRotation = rootPart.rotation;
+        parts[0][0] = rootPart; //copy back to array
+
+        for (int li = 1; li < parts.Length; li++) //traverse every level of the parts array (skipping 0, as the root obj never moves)
+        {
+            //keep references to the current part and it's parent
+            FractalPart[] parentParts = parts[li - 1];
+            FractalPart[] levelParts = parts[li];
+            for (int fpi = 0; fpi < levelParts.Length; fpi++)// traverse every fractal part on the level
+            {
+                Transform parentTransform = parentParts[fpi / 5].transform; // get the parent part's transform
+                FractalPart part = levelParts[fpi];
+                part.rotation *= deltaRotation;
+                part.transform.localRotation = parentTransform.localRotation * part.rotation; //Quaternion multiplication performs the second rotation, followed by the first rotation so order matters
+                part.transform.localPosition = parentTransform.position + 
+                    parentTransform.localRotation * 
+                    (1.5f * part.transform.localScale.x * part.direction); //set position using scale and direction, applying parents rotation too.
+                levelParts[fpi] = part;
             }
         }
     }
