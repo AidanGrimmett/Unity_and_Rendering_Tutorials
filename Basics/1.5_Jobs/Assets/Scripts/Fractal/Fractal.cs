f9ac39821220e9b60e96d4f1944d512c753ef4db
AidanGrimmettSwin <141190734+AidanGrimmettSwin@users.noreply.github.com>
2025-01-04 11:00:30 +1100

    working through the procedural drawing updates, shader should be complete? Continuing from 3.5 drawing

--- Basics/1.5_Jobs/Assets/Scripts/Fractal/Fractal.cs
+++ Basics/1.5_Jobs/Assets/Scripts/Fractal/Fractal.cs
@@ -5,9 +5,10 @@ public class Fractal : MonoBehaviour
 
     struct FractalPart
     {
-        public Vector3 direction;
-        public Quaternion rotation;
-        public Transform transform;
+        public Vector3 direction, worldPosition;
+        public Quaternion rotation, worldRotation;
+        public float spinAngle; //used to maintain fresh quaternions for transformation matrices,
+                         //floating point errors plague us otherwise.
     }
 
     [SerializeField, Range(1, 8)]
@@ -21,6 +22,9 @@ public class Fractal : MonoBehaviour
 
     FractalPart[][] parts;
 
+    Matrix4x4[][] matrices; //usually, the transform component contains the necessary transformation matrices
+                            //as we are now using procedural drawing instead of GOs, we need to store these manually.
+
     static Vector3[] directions = { Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back };
 
     static Quaternion[] rotations = 
@@ -30,77 +34,103 @@ public class Fractal : MonoBehaviour
         Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f) 
     };
 
-    FractalPart CreatePart(int levelIndex, int childIndex, float scale)
+    ComputeBuffer[] matricesBuffers;
+
+    FractalPart CreatePart(int childIndex) => new FractalPart
     {
-        GameObject part = new GameObject("Fractal Part L" + levelIndex + " C" + childIndex);
-        part.transform.localScale = scale * Vector3.one;
-        part.transform.SetParent(transform);
-        part.AddComponent<MeshFilter>().mesh = mesh;
-        part.AddComponent<MeshRenderer>().material = material;
+        direction = directions[childIndex],
+        rotation = rotations[childIndex]
+    };
 
-        return new FractalPart
-        {
-            direction = directions[childIndex],
-            rotation = rotations[childIndex],
-            transform = part.transform
-        };
-    }
-
-    private void Awake()
+    private void OnEnable()
     {
         //set up the parts array
         //parts is an array of FractalPart[] arrays, with `depth` elements (ie, (int)depth FractalPart[] arrays)
         parts = new FractalPart[depth][];
+        matrices = new Matrix4x4[depth][];
+        matricesBuffers = new ComputeBuffer[depth];
+        int stride = 16 * 4; //4x4 matrix has 16 floats, so 16 * 4 stride length
 
         for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
         {
             parts[i] = new FractalPart[length];
+            matrices[i] = new Matrix4x4[length];
+            matricesBuffers[i] = new ComputeBuffer(length, stride);
         }//each level of the parts array has a FractalPart[] which has 5 times the points as the last level
 
-        float scale = 1f;
-        parts[0][0] = CreatePart(0, 0, scale);
+        parts[0][0] = CreatePart(0);
+
         //li = level index
         //fpi = fractal part index
         //ci = child index
         for (int li = 1; li < parts.Length; li++)
         {
-            scale *= 0.5f; //reduce scale by half each level
             FractalPart[] levelParts = parts[li]; //collection of parts on this level
             for (int fpi = 0; fpi < levelParts.Length; fpi += 5) //iterate over all parts on level, increases by 5 as we have another loop which wil create 5 parts
             {
                 for (int ci = 0; ci < 5; ci++) //create 5 parts, child index will help distinguish between directions
                 {
-                    levelParts[fpi + ci] = CreatePart(li, ci, scale);
+                    levelParts[fpi + ci] = CreatePart(ci);
                 }
             }
         }
     }
 
+    private void OnDisable()
+    {
+        for (int i = 0; i < matricesBuffers.Length; i++)
+        {
+            matricesBuffers[i].Release();
+        }
+        parts = null;
+        matrices = null;
+        matricesBuffers = null;
+    }
+
+    private void OnValidate()
+    {
+        if (parts !=null && enabled)
+        {
+            OnDisable();
+            OnEnable();
+        }
+    }
+
     private void Update()
     {
-        Quaternion deltaRotation = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+        float spinAngleDelta = 22.5f * Time.deltaTime;
 
         FractalPart rootPart = parts[0][0];
-        rootPart.rotation *= deltaRotation; //changing local struct variable value will not change the array element
-        rootPart.transform.localRotation = rootPart.rotation;
-        parts[0][0] = rootPart; //copy back to array
-
+        rootPart.spinAngle += spinAngleDelta; //changing local struct variable value will not change the array element
+        rootPart.worldRotation = rootPart.rotation * Quaternion.Euler(0f, rootPart.spinAngle, 0f);
+        parts[0][0] = rootPart; //copy back to array to update
+        matrices[0][0] = Matrix4x4.TRS(rootPart.worldPosition, rootPart.worldRotation, Vector3.one);
+        float scale = 1f;
         for (int li = 1; li < parts.Length; li++) //traverse every level of the parts array (skipping 0, as the root obj never moves)
         {
+            scale *= 0.5f;
             //keep references to the current part and it's parent
             FractalPart[] parentParts = parts[li - 1];
             FractalPart[] levelParts = parts[li];
+            Matrix4x4[] levelMatrices = matrices[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi++)// traverse every fractal part on the level
             {
-                Transform parentTransform = parentParts[fpi / 5].transform; // get the parent part's transform
+                FractalPart parent = parentParts[fpi / 5];
                 FractalPart part = levelParts[fpi];
-                part.rotation *= deltaRotation;
-                part.transform.localRotation = parentTransform.localRotation * part.rotation; //Quaternion multiplication performs the second rotation, followed by the first rotation so order matters
-                part.transform.localPosition = parentTransform.position + 
-                    parentTransform.localRotation * 
-                    (1.5f * part.transform.localScale.x * part.direction); //set position using scale and direction, applying parents rotation too.
+                part.spinAngle += spinAngleDelta;
+                part.worldRotation = parent.worldRotation * (part.rotation * Quaternion.Euler(0f, part.spinAngle, 0f)); //Quaternion multiplication performs the second rotation, followed by the first rotation so order matters
+                part.worldPosition = parent.worldPosition + 
+                    parent.worldRotation * 
+                    (1.5f * scale * part.direction); //set position using scale and direction, applying parents rotation too.
                 levelParts[fpi] = part;
+
+                levelMatrices[fpi] = Matrix4x4.TRS(part.worldPosition, part.worldRotation, scale * Vector3.one);
             }
+        }
+
+        for (int i = 0; i < matricesBuffers.Length; i++)
+        {
+            matricesBuffers[i].SetData(matrices[i]);
         }
     }
 }
