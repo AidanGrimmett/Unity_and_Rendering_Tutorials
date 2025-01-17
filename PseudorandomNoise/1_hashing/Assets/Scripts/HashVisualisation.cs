544d4d775fc92641f17fdd0098a86d30b6f34ca3
AidanGrimmett <aids.grimmett@gmail.com>
2025-01-17 16:27:48 +1100

    first commit for pseudo random noise tutorials.
    created project for first tutorial
    installed burst and URP packages, set up project to use URP.
    
    Created HashVisualisation script, basic job function
    
    continue from 1.2 after learning more about NativeArrays / native memory
    / managed code

---
+++ PseudorandomNoise/1_hashing/Assets/Scripts/HashVisualisation.cs
@@ -0,0 +1,11 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+using static Unity.Mathematics.math;
+public class HashVisualisation : MonoBehaviour
+{
+
+}
